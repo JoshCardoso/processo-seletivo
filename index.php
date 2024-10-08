ca569eac87ac419c03f9a2d81321698bb9ada55e
Joshua Cardoso <joshuacardoso97@gmail.com>
2024-10-07 21:32:44 -0300

    teste 1

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+//processo Seletivo
+
+//Teste 1
+
+$indece = 13;
+$soma = 0;
+$k = 0;
+
+while ($k < $indece){
+    $k = $k + 1; 
+    $soma = $soma + $k;
+    
+}
+
+echo "o valor final da Variavel Soma é " . $soma;
+
+
