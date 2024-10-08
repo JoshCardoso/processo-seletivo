fd99c5d271123a5d7cb7eb6da8c3805c8c9ee0bc
Joshua Cardoso <joshuacardoso97@gmail.com>
2024-10-07 23:11:49 -0300

    teste 2

--- index.php
+++ index.php
@@ -15,4 +15,25 @@ while ($k < $indece){
 
 echo "o valor final da Variavel Soma é " . $soma;
 
+echo"<br/>";
+// teste 2
 
+function raiz_quadrado_exata ($n) {
+    $raiz = sqrt($n);
+    return $raiz == intval($raiz);
+}
+
+function pertence_fibonacci($n) {
+    
+    $teste1 = 5 * pow($n, 2) + 4;
+    $teste2 = 5 * pow($n, 2) - 4;
+    
+    return raiz_quadrado_exata($teste1) || raiz_quadrado_exata($teste2);
+}
+
+$numero = 5; 
+if (pertence_fibonacci($numero)) {
+    echo "$numero pertence à sequência de Fibonacci.";
+} else {
+    echo "$numero não pertence à sequência de Fibonacci.";
+}
