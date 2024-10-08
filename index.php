2ac27463d202e154f926ab6ccc238254fa300209
Joshua Cardoso <joshuacardoso97@gmail.com>
2024-10-08 00:29:51 -0300

    teste 3

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 //processo Seletivo
 
 //Teste 1
+echo"Teste 1";
+echo"<br>";
 
 $indece = 13;
 $soma = 0;
@@ -15,8 +17,11 @@ while ($k < $indece){
 
 echo "o valor final da Variavel Soma é " . $soma;
 
-echo"<br/>";
+echo"<br>";
+echo"<br>";
 // teste 2
+echo"Teste 2";
+echo"<br>";
 
 function raiz_quadrado_exata ($n) {
     $raiz = sqrt($n);
@@ -37,3 +42,50 @@ if (pertence_fibonacci($numero)) {
 } else {
     echo "$numero não pertence à sequência de Fibonacci.";
 }
+
+echo"<br>";
+echo"<br>";
+
+// teste 3
+echo"Teste 3";
+echo"<br>";
+
+$json = file_get_contents('dados.json');
+$faturamentos = json_decode($json, true);
+
+$menorFaturamento = PHP_FLOAT_MAX;
+$maiorFaturamento = PHP_FLOAT_MIN;
+$somaFaturamento = 0;
+$diasComFaturamento= 0;
+$acimaMedia = 0;
+
+foreach ($faturamentos as $faturamento) {
+    if ($faturamento["valor"] > 0) { 
+        $diasComFaturamento++;
+        $somaFaturamento += $faturamento["valor"];
+        
+        if ($faturamento["valor"] < $menorFaturamento) {
+            $menorFaturamento = $faturamento["valor"];
+        }
+        
+        if ($faturamento["valor"] > $maiorFaturamento) {
+            $maiorFaturamento = $faturamento["valor"];
+        }
+    }
+}
+
+$mediaMensal = $somaFaturamento / $diasComFaturamento;
+
+foreach ($faturamentos as $faturamento) {
+    if ($faturamento["valor"] > $mediaMensal) {
+        $acimaMedia++;
+    }
+}
+
+echo "Menor valor de faturamento: R$ " . number_format($menorFaturamento, 2, ',', '.') . "<br>";
+echo "Maior valor de faturamento: R$ " . number_format($maiorFaturamento, 2, ',', '.') . "<br>";
+echo "Número de dias com faturamento acima da média mensal: $acimaMedia<br>";
+
+echo"<br>";
+echo"Teste 4";
+echo"<br>";
